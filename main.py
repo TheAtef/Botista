941e2b4fcede821744d7b6cb97f055f9ddd26d0b
TheAtef <69214639+TheAtef@users.noreply.github.com>
2024-01-19 22:25:33 +0300

    remove product_type

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from instagrapi import Client
 from datetime import datetime
 import requests
 import json
+import time
 from server import server
 
 headers = {
@@ -73,11 +74,10 @@ def start(message):
                 followers = parsed_json['follower_count']
                 followings = parsed_json['following_count']
                 bio = parsed_json['biography']
-                caption = f"Name: {name}\nUsername: <a href='{INSTA_URL}{username}/'>{username}</a>\nFollowers: {followers}\nFollowing: {followings}\nBio:\n{bio}"
+                caption = f"Name: {name}\nUsername: <a href='{INSTA_URL}{username}/'>@{username}</a>\nFollowers: {followers}\nFollowing: {followings}\nBio:\n{bio}"
                 pic = parsed_json['hd_profile_pic_url_info']['url']
                 bot.send_photo(message.chat.id, pic, caption, reply_to_message_id=message.message_id, parse_mode = 'HTML')
-        except Exception as e:
-            print(e)
+        except Exception:
             bot.reply_to(message, "Unvalid username.")
 
 @bot.message_handler(func=lambda m: True)
@@ -91,27 +91,24 @@ def get_media(message):
         if r.status_code == 200:
             try:
                 parsed_json = json.loads(r.text)["items"].pop()
-                parsed_json.pop('facepile_top_likers', None)
-                parsed_json.pop('top_likers', None)
-                product_type = parsed_json['product_type']
                 media_type = parsed_json['media_type']
             except Exception:
-                product_type = media_type = None
+                media_type = None
                 bot.reply_to(message, "Sorry, something went wrong.")
         else:
             bot.delete_message(message.chat.id, delete.message_id, timeout=10)
-            product_type = media_type = None
+            media_type = None
             bot.reply_to(message, "Invalid, please send a public Instagram link.")
     else:
         bot.delete_message(message.chat.id, delete.message_id, timeout=10)
-        product_type = media_type = None
+        media_type = None
         bot.reply_to(message, "Invalid, please send a public Instagram link.")
     
-    if product_type == "clips" or product_type == "igtv":
-        video_url = sorted(parsed_json["video_versions"], key=lambda o: o["height"] * o["width"])[-1]["url"]
-        caption = f"User: <a href='{INSTA_URL}{parsed_json['user']['username']}/'>@{parsed_json['user']['username']}</a>\n\nCaption:\n{parsed_json['caption']['text']}\n\nPlays: {parsed_json['play_count']:,}\nLikes: {parsed_json['like_count']:,}"
+    if media_type == 2:
         bot.delete_message(message.chat.id, delete.message_id, timeout=10)
         bot.send_chat_action(message.chat.id, action='upload_video')
+        video_url = sorted(parsed_json["video_versions"], key=lambda o: o["height"] * o["width"])[-1]["url"]
+        caption = f"User: <a href='{INSTA_URL}{parsed_json['user']['username']}/'>@{parsed_json['user']['username']}</a>\n\nCaption:\n{parsed_json['caption']['text']}\n\nPlays: {parsed_json['play_count']:,}\nLikes: {parsed_json['like_count']:,}"
         try:
             if len(caption) <= 1024:
                 bot.send_video(chat_id=message.chat.id, video=video_url, timeout=200, caption=caption, reply_to_message_id=message.message_id, parse_mode='HTML')
@@ -122,10 +119,10 @@ def get_media(message):
             bot.reply_to(message, "Sorry, something went wrong.")
 
     if media_type == 1:
-        image = sorted(parsed_json["image_versions2"]["candidates"],key=lambda o: o["height"] * o["width"],)[-1]["url"]
-        caption = f"User: <a href='{INSTA_URL}{parsed_json['user']['username']}/'>@{parsed_json['user']['username']}</a>\n\nCaption:\n{parsed_json['caption']['text']}\n\nLikes: {parsed_json['like_count']:,}"
         bot.delete_message(message.chat.id, delete.message_id, timeout=10)
         bot.send_chat_action(message.chat.id, action='upload_photo')
+        image = sorted(parsed_json["image_versions2"]["candidates"],key=lambda o: o["height"] * o["width"],)[-1]["url"]
+        caption = f"User: <a href='{INSTA_URL}{parsed_json['user']['username']}/'>@{parsed_json['user']['username']}</a>\n\nCaption:\n{parsed_json['caption']['text']}\n\nLikes: {parsed_json['like_count']:,}"
         try:
             if len(caption) <= 1024:
                 bot.send_photo(chat_id=message.chat.id, photo=image, timeout=200, caption=caption, reply_to_message_id=message.message_id, parse_mode='HTML')
@@ -159,5 +156,6 @@ print('Bot is running...')
 while True:
     try:
         bot.infinity_polling()
-    except:
+    except Exception as e:
+        print(e)
         time.sleep(10)
